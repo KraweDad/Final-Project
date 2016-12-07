83a76260d2a919ca75bcec9ab14b62126b856359
KraweDad <mattfenner35@gmail.com>
2016-12-07 13:15:30 -0500

    Updated from Brython Server: 12/7/2016 1:15:30 PM

--- Zoxy.py
+++ Zoxy.py
@@ -4,11 +4,11 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 940
 
-class Platformer(App):
+class Zoxy(App):
     def __init__(self, width, height):
         super().__init__(width, height)
         black = Color(1, 1)
-        line = LineStyle(3, black)
+        line = LineStyle(2, black)
         grass = Color(0x229954, 1)
         hedge = Color(0x145A32, 1)
         stone = Color(0xB2BABB, 1)
@@ -16,16 +16,22 @@ class Platformer(App):
         roof = Color(0xA04000, 1)
         Grass = RectangleAsset(1279,939,line,grass)
         Hedge = RectangleAsset(25,700,line,hedge)
-        Stone = RectangleAsset(50,20,line,stone)
+        Hedge2 = RectangleAsset(25,340,line,hedge)
+        Hedge3 = RectangleAsset(25,290,line,hedge)
+        Hedge4 = RectangleAsset(485,25,line,hedge)
+        Stone = RectangleAsset(20,50,line,stone)
         Road = RectangleAsset(60,940,line,road)
         Roof = RectangleAsset(300,400,line,roof)
         Sprite(Grass, (0,0))
         Sprite(Roof, (440,270))
-        Sprite(Stone, (480,690))
-        Sprite(Stone, (480,740))
+        Sprite(Stone, (420,520))
+        Sprite(Stone, (380,520))
         Sprite(Road, (320,0))
-        Sprite(Hedge, (1000,500))
-        
+        Sprite(Hedge, (840,170))
+        Sprite(Hedge2, (380, 170))
+        Sprite(Hedge3, (380, 580))
+        Sprite(Hedge4, (380, 845))
+        Sprite(Hedge4, (380, 145))
 
-myapp = Platformer(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp = Zoxy(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
