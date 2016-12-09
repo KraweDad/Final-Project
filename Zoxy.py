aa311bca00cb1955778fa80c55ba10fe10e5b25d
KraweDad <mattfenner35@gmail.com>
2016-12-09 13:59:58 -0500

    Updated from Brython Server: 12/9/2016 1:59:58 PM

--- Zoxy.py
+++ Zoxy.py
@@ -4,6 +4,48 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 940
 
+class Bingo(Sprite):
+    black = Color(0,1)
+    skin = Color(0x003300, 1)
+    line = LineStyle(0, black)
+    asset = RectangleAsset(20,20, line, skin)
+    
+    def __init__(self,position):
+        super().__init__(Bingo.asset, position)
+        self.vy = 0
+        self.vx = 0
+        self.vr = 0
+        self.fxcenter = self.fycenter = 0
+        Zoxy.listenKeyEvent("keydown", "up arrow", self.movenorth)
+        Zoxy.listenKeyEvent("keyup", "up arrow", self.moveoff)
+        Zoxy.listenKeyEvent("keydown", "down arrow", self.movesouth)
+        Zoxy.listenKeyEvent("keyup", "down arrow", self.moveoff)
+        Zoxy.listenKeyEvent("keydown", "right arrow", self.moveeast)
+        Zoxy.listenKeyEvent("keyup", "right arrow", self.moveoff)
+        Zoxy.listenKeyEvent("keydown", "left arrow", self.movewest)
+        Zoxy.listenKeyEvent("keyup", "left arrow", self.moveoff)
+        
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        
+    def movenorth(self,event):
+        self.vy = -2
+        
+    def movesouth(self,event):
+        self.vy = 2
+    
+    def moveeast(self,event):
+        self.vx = 2
+    
+    def movewest(self,event):
+        self.vx = -2
+    
+    def moveoff(self,event):
+        self.vx = self.vy = 0
+    
+        
+
 class Zoxy(App):
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -26,12 +68,17 @@ class Zoxy(App):
         Sprite(Roof, (440,270))
         Sprite(Stone, (420,520))
         Sprite(Stone, (380,520))
-        Sprite(Road, (320,0))
+        Sprite(Road, (310,0))
         Sprite(Hedge, (840,170))
         Sprite(Hedge2, (380, 170))
         Sprite(Hedge3, (380, 580))
         Sprite(Hedge4, (380, 845))
         Sprite(Hedge4, (380, 145))
+        Bingo((350, 900))
+        
+    def step(self):
+        for ship in self.getSpritesbyClass(Bingo):
+            ship.step()
 
 myapp = Zoxy(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
